7fc6c66a6f7f9b8137919cfea1240b528c7c54e0
Nahin-CDR <dev.nahin95@gmail.com>
2020-08-27 01:39:12 +0600

    first Commit

---
+++ app/src/main/java/com/nahinthecoder/apilearning/JsonPlaceHolderApi.java
@@ -0,0 +1,15 @@
+package com.nahinthecoder.apilearning;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface JsonPlaceHolderApi {
+
+    @GET("posts")
+    Call<List<Post>> getPost();
+
+
+
+}
